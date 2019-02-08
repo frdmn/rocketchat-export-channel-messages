c548810df502a4708b00bffa07ee11f634dbebe2
Jonas Friedmann <j@frd.mn>
2019-02-08 14:46:03 +0100

    Check for mandatory --channel option

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ program
     .description(packagejson.description)
     .option('-j, --json', 'Export as JSON file rather than CSV')
     .parse(process.argv);
+// Check for mandantory "-c" flag
+if (!program.channel) {
+    program.outputHelp(() => program.help());
+}
+
